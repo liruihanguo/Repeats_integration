01851c5c870d745190bf24f1f1912b6c5536b3ef
liruihanguo <86306810+liruihanguo@users.noreply.github.com>
2021-08-30 11:44:46 +0800

    Update work.sh

--- work.sh
+++ work.sh
@@ -3,4 +3,4 @@ cat denovo.gff trf.gff repeatmasker.gff proteinmask.gff  > all.gff
 perl merge_overlap.pl all.gff >merge_overlap.gff
 ##classify each kind of repeats##
 cat denovo.gff repeatmasker.gff proteinmask.gff  > all_without_trf.gff
-perl stat.pl -denovo -trf -repeatmasker -proteinmask  Sino_maitian.fa 
+perl stat.pl -denovo -trf -repeatmasker -proteinmask  Genome.fa 
